a35a6ac2b815e09eb96223c87a8d685476eab097
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-02-23 18:21:20 +0000

    rewritten tag handler as a restricted  iterator tag

--- trunk/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/tag/BreadCrumbTag.java
+++ trunk/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/tag/BreadCrumbTag.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2011 - Giovanni Tosto
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */   
 package org.softwareforge.struts2.breadcrumb.tag;
 
 import javax.servlet.http.HttpServletRequest;
@@ -5,18 +20,46 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
 
 import org.apache.struts2.components.Component;
+import org.apache.struts2.components.IteratorComponent;
 import org.apache.struts2.views.jsp.ContextBeanTag;
+import org.softwareforge.struts2.breadcrumb.BreadCrumbInterceptor;
 
 import com.opensymphony.xwork2.util.ValueStack;
 
+/**
+ * @author Giovanni Tosto
+ * @version $Id$
+ */
 public class BreadCrumbTag extends ContextBeanTag {
-
-	@Override
-	public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {		
-		return new BreadCrumbComponent(stack,req);
+	
+	private static final long serialVersionUID = 1L;
+	
+	// Status attribute
+	private String	status;
+	
+	public String getStatus() {
+		return status;
 	}
 
-    public int doEndTag() throws JspException {
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+		IteratorComponent ic = new IteratorComponent(stack);
+		return ic;
+	}
+
+    @Override
+	protected void populateParams() {
+		super.populateParams();
+        IteratorComponent c = (IteratorComponent) getComponent();
+		c.setValue("#session['" + BreadCrumbInterceptor.CRUMB_KEY + "']");
+		c.setStatus(status);
+	}
+
+	public int doEndTag() throws JspException {
         component = null;
         return EVAL_PAGE;
     }
