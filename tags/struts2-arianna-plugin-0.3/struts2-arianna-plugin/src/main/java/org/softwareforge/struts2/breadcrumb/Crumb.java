4bb3e2df4fcc2e7657e383d9ae25d26ca6faa08e
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-02-24 21:36:55 +0000

    [maven-release-plugin]  copy for tag struts2-arianna-plugin-0.3

---
+++ tags/struts2-arianna-plugin-0.3/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/Crumb.java
@@ -0,0 +1,107 @@
+/*
+ *  Copyright 2011 - Giovanni Tosto
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */   
+package org.softwareforge.struts2.breadcrumb;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ *  @author Giovanni Tosto
+ *  @version $Id$
+ */
+
+public class Crumb implements Serializable
+{
+	String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	String action;
+
+	public String getAction() {
+		return action;
+	}
+
+	public String getNamespace() {
+		return namespace;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	String namespace;
+	
+	String method;
+	
+	Map	params;
+	
+	public String getFullyQualifiedId() {
+		return namespace + "/" + action + "!" + method + ":" + name;		
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((namespace == null) ? 0 : namespace.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Crumb other = (Crumb) obj;
+//		if (action == null) {
+//			if (other.action != null)
+//				return false;
+//		} else if (!action.equals(other.action))
+//			return false;
+//		if (method == null) {
+//			if (other.method != null)
+//				return false;
+//		} else if (!method.equals(other.method))
+//			return false;
+//		if (name == null) {
+//			if (other.name != null)
+//				return false;
+//		} else if (!name.equals(other.name))
+//			return false;
+//		if (namespace == null) {
+//			if (other.namespace != null)
+//				return false;
+//		} else if (!namespace.equals(other.namespace))
+//			return false;
+		return getFullyQualifiedId().equals(other.getFullyQualifiedId());
+	}
+	
+	
+}
---
+++ tags/struts2-arianna-plugin-0.3/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/ReflectionUtils.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright 2011 - Giovanni Tosto
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */   
+package org.softwareforge.struts2.breadcrumb;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ * 
+ *	@author Giovanni Tosto
+ *	@version $Id$
+ */
+public class ReflectionUtils 
+{
+	static final Log LOG = LogFactory.getLog(ReflectionUtils.class);
+	
+	@SuppressWarnings("unchecked")
+	public static Method	findMethod(Class clazz, String name) {
+		
+		try {
+			return clazz.getMethod(name, (Class[])null);
+		} catch (SecurityException e) {
+			return null;
+		} catch (NoSuchMethodException e) {
+			return null;
+		}
+	}
+}
