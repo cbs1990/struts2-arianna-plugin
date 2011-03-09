784bb10c9cef73db583fae2201aad33efea7063a
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-03-09 19:09:42 +0000

    fixed comparator conversion

--- trunk/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/CrumbComparatorConverter.java
+++ trunk/struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/CrumbComparatorConverter.java
@@ -15,11 +15,15 @@
  */   
 package org.softwareforge.struts2.breadcrumb;
 
+import java.util.Comparator;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.util.StrutsTypeConverter;
+
+import com.opensymphony.xwork2.ObjectFactory;
+import com.opensymphony.xwork2.conversion.TypeConversionException;
 
 /**
  * 
@@ -33,6 +37,27 @@ public class CrumbComparatorConverter extends StrutsTypeConverter
 	
 	public Object convertFromString(Map context, String[] values, Class toClass) 
 	{
+		if (values == null )
+			return null;
+		
+		if ( values.length != 1 ) {
+			throw new TypeConversionException("Unable to convert non scalar value " + values);			
+		}
+				
+		if ( !toClass.isAssignableFrom(Comparator.class) ) {
+			throw new TypeConversionException("class " + toClass + " is not assignable to Comparator.class");			
+		}
+		
+		try {
+			Class clazz = ObjectFactory.getObjectFactory().getClassInstance(values[0]);
+			if ( Comparator.class.isAssignableFrom(clazz) ) {
+				Comparator instance = (Comparator) clazz.newInstance();
+				return instance;
+			}
+		} catch (Exception e) {
+			throw new TypeConversionException(e.getMessage(),e);
+		}
+		
 		Object obj = new NameComparator();
 		if ( LOG.isDebugEnabled() )
 		{
@@ -46,7 +71,6 @@ public class CrumbComparatorConverter extends StrutsTypeConverter
 	
 	public String convertToString(Map context, Object o) 
 	{
-		LOG.warn("convertToString is not supported by this Converter");
-		return "" + o.toString();
+		return o == null ? null : o.getClass().getName();
 	}
 }
