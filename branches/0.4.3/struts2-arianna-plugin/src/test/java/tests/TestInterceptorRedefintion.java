1c236e5e9e9d63ca4d7bce2c738f860ee0d28260
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-04-12 21:37:25 +0000

    new test cases

---
+++ branches/0.4.3/struts2-arianna-plugin/src/test/java/tests/TestInterceptorRedefintion.java
@@ -0,0 +1,31 @@
+package tests;
+
+import java.util.HashMap;
+
+import org.softwareforge.struts2.breadcrumb.BreadCrumbTrail;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+public class TestInterceptorRedefintion extends BreadcrumbTestCase {
+
+    public void testSimpleAction() throws Exception {
+	sessionMap = new HashMap();
+		
+	ActionProxy ap1 = getActionProxy("/R/inspect1.do");		
+	String rc1 = ap1.execute();
+	
+	ActionProxy ap2 = getActionProxy("/R/inspect1.do");		
+	String rc2 = ap2.execute();	
+    }
+
+    public void testActionRedefinition() throws Exception {
+	sessionMap = new HashMap();
+		
+	ActionProxy ap1 = getActionProxy("/R/inspect1.do");		
+	String rc1 = ap1.execute();
+	
+	ActionProxy ap2 = getActionProxy("/R/inspect2.do");		
+	String rc2 = ap2.execute();	
+    }
+    
+}
