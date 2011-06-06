d4a828748c39d76b73ca803385ea12aa64584f7b
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-06-06 16:52:02 +0000

    [maven-release-plugin]  copy for tag struts2-arianna-plugin-0.5.1b2

---
+++ struts2-arianna-plugin/src/main/java/org/softwareforge/struts2/breadcrumb/AriannaPlugin.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2011 - Giovanni Tosto
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */ 
+
+package org.softwareforge.struts2.breadcrumb;
+
+import java.util.Comparator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.opensymphony.xwork2.inject.Inject;
+
+public class AriannaPlugin {
+    
+    private static final Log LOG = LogFactory.getLog(BreadCrumbInterceptor.class);
+    
+    int maxCrumbs = 6;
+    
+    public AriannaPlugin() {
+	LOG.info("Initializing Arianna Plugin : " + this);
+    }
+    
+    @Inject("arianna:maxCrumbs")
+    protected void setMaxCrumbs(String v) {
+	maxCrumbs = Integer.parseInt(v);	
+    }
+    
+    public String	getVersion() {
+	return getClass().getPackage().getSpecificationVersion();
+    }
+	
+    public String	getTitle() {
+	return getClass().getPackage().getSpecificationTitle();
+    }
+ 
+    
+    public	int	getDefaultMaxCrumbs() {
+	
+	return maxCrumbs;
+    }
+
+    protected Comparator<Crumb> lookupComparatorByClass(Class clazz) {
+	
+	try {
+	    Comparator instance = (Comparator) clazz.newInstance();
+	    return instance;
+	} catch (ClassCastException e) {
+	    LOG.error(clazz + " is not assignable to " + Comparator.class,e);
+	} catch (InstantiationException e) {
+	    LOG.error("Cannot create comparator of class " + clazz, e);
+	} catch (IllegalAccessException e) {
+	    LOG.error("Cannot create comparator of class " + clazz, e);
+	}
+	
+	return null;
+
+    }
+    
+    protected Comparator<Crumb> lookupComparatorByClass(String className) {
+	
+	try {
+	    Class<?> clazz = Class.forName(className);
+	    return lookupComparatorByClass(clazz);
+	} catch (ClassNotFoundException e) {
+	    LOG.error("Cannot create comparator of class " + className, e);
+	    return null;
+	}
+    }
+    
+    public static void main(String[] args) {
+	System.out.println("This is the Struts2 Arianna Plugin");
+	AriannaPlugin plugin = new AriannaPlugin();
+	String version = plugin.getVersion();
+	System.out.println("Version: " + version);
+    }
+}
---
+++ struts2-arianna-plugin/src/test/java/tests/TestOverrideComparator.java
@@ -0,0 +1,43 @@
+package tests;
+
+import java.util.HashMap;
+
+import org.apache.struts2.StrutsTestCase;
+import org.apache.struts2.config.StrutsXmlConfigurationProvider;
+import org.softwareforge.struts2.breadcrumb.BreadCrumbInterceptor;
+import org.softwareforge.struts2.breadcrumb.BreadCrumbTrail;
+
+import com.opensymphony.xwork2.ActionProxy;
+import com.opensymphony.xwork2.config.ConfigurationProvider;
+import com.opensymphony.xwork2.util.profiling.UtilTimerStack;
+
+public class TestOverrideComparator extends BreadcrumbTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+	UtilTimerStack.setActive(true);
+	System.setProperty("xwork.profile.activate", "true");
+	// TODO Auto-generated method stub
+	super.setUp();
+    }
+
+    public void testOverridingAction() throws Exception {
+	sessionMap = new HashMap();
+		
+	ActionProxy proxy = getActionProxy("/c-OverrideComparatorAction.do");		
+	String rc = proxy.execute();
+		
+	BreadCrumbTrail breadCrumbTrail = getBreadCrumbTrail(sessionMap);
+		
+	/*
+	 * assert that a breadcrumb trail has been stored 
+	 * and that it contains at least a crumb
+	 */
+	
+	assertNotNull("No breadcrumb trail in session", breadCrumbTrail);
+	assertTrue(breadCrumbTrail.getCrumbs().size() > 0);
+		
+	System.out.printf("crumbs: %s", breadCrumbTrail.getCrumbs());
+    }
+	
+}
