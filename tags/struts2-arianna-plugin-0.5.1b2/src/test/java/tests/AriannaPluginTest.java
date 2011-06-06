0120fb76ad86a7bf3ede976c2ff06b5dae9adfea
giovanni.tosto <giovanni.tosto@5e5cafbd-26eb-801f-e8f1-4dc568ae50c4>
2011-06-06 16:47:22 +0000

    [maven-release-plugin]  copy for tag struts2-arianna-plugin-0.5.1b2

---
+++ tags/struts2-arianna-plugin-0.5.1b2/src/test/java/tests/AriannaPluginTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import junit.framework.Assert;
+
+import org.softwareforge.struts2.breadcrumb.AriannaPlugin;
+
+import com.opensymphony.xwork2.ObjectFactory;
+
+public class AriannaPluginTest extends BreadcrumbTestCase {
+
+    public	void testPluginVersion() {
+	AriannaPlugin plugin = configuration.getContainer().getInstance(AriannaPlugin.class,"arianna");
+	
+	Assert.assertNotNull("AriannaPlugin NOT FOUND",plugin);
+	
+	int maxCrumbs = plugin.getDefaultMaxCrumbs();
+//	assertTrue(maxCrumbs == 6);
+	
+	String version = plugin.getVersion();
+	String name = plugin.getTitle();
+	
+	
+//	Assert.assertNotNull(version);
+//	Assert.assertNotNull(name);
+    }
+}
